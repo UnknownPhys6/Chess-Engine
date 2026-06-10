edc4165d3c5462ba8858f83a484c6779adb27bb0
UnknownPhys6 <andyniemerg@gmail.com>
2026-06-09 19:39:24 -0500

    added comment

--- main.cpp
+++ main.cpp
@@ -184,6 +184,7 @@ class Chess {
         }
 
         //White Bishop
+        //currently bugged
         void list_White_Bishop_Moves() {
             for ( int x=0; x<8; x++) {
                 for (int y=0; y<8; y++) {
